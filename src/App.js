3d5cca22a366a261ef8d3ac35b761ea18399cdd9
abernklau1 <andrewbernklau@hotmail.com>
2022-08-25 11:55:13 -0600

    Correct error page url

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
-        <Route path="/*" element={<Error />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
